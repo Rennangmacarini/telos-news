63a99a3c09543c49e9826ac958a52424ce845ed3
Rennan Macarini <rennangmacarini@gmail.com>
2023-04-24 00:40:36 -0300

    feat: `:package: `global commit on styling and packege

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { Header } from './components/Header';
-import Home from './pages/Home';
+import { Home } from './pages/Home';
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <div className="mainScroll"> 
       <Home/>
+      </div>
     </div>
   );
 }
