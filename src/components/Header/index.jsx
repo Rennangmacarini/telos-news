7f4913efe2e6a6428c161204db6cc71026fcdf26
Rennan Macarini <rennangmacarini@gmail.com>
2023-04-24 00:26:51 -0300

    feat: `:fire:`creating navigation menu in component header using material ui

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import logo from './caminho.png';
+import CreateAccountButton from '../createAccountButton';
 
-export function Header(){
-  return(
-    <h1>Olá</h1>
-  )
+export function Header() {
+  return (
+    <Box sx={{ flexGrow: 1}}>
+      <AppBar position="static"
+      sx={{ background: "#fff", boxShadow: "none", border: "1px solid rgba(0, 0, 0, 0.07)", padding: "10px 120px"}}
+      >
+        <Toolbar 
+        sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}
+        >
+          <Typography variant="h6" component="div" sx={{ display: "flex", alignItems: "center", gap: "4px", color: "#000" }}>
+            <img src={logo} alt="" />
+            Télos
+          </Typography>
+          <CreateAccountButton/>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
-  
-
