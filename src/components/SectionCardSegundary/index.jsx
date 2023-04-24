022e8c58c20c22e5ea9108450d8cc4ec98ad00f1
Rennan Macarini <rennangmacarini@gmail.com>
2023-04-24 00:32:54 -0300

    feat: `:sparkles: ` added my second card section

---
+++ src/components/SectionCardSegundary/index.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import Logo from "./image-46.png"
+
+export default function SectionCardSegundary({button}) {
+
+  return (
+    <Card sx={{ display: 'flex', alignItems: "flex-end", backgroundColor: "#FEF7FF;" , borderRadius: "38px"}}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', padding: "0px 140px 0px 40px" }}>
+        <Button sx={{ padding: "15px 30px", backgroundColor: "#6750A4", color: "white", fontWeight: "bold", borderRadius: "140px", marginBottom: "40px"}} href="#contained-buttons">
+          {button}
+        </Button>
+      </Box>
+      <CardMedia
+        component="img"
+        sx={{ width: 570 }}
+        image={Logo}
+        alt="Live from space album cover"
+      />
+    </Card>
+  );
+}
