bb10a3d432d1a953eebd6e2fbe471dc80f5f8cc4
Rennan Macarini <rennangmacarini@gmail.com>
2023-04-24 17:36:03 -0300

    feat: `:fire: ` adding news page

---
+++ src/pages/Noticia/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { MainNoticia } from "../../components/mainNoticia";
+
+export function Noticia(){
+    return(
+        <>
+        <MainNoticia/>
+        </>
+    )
+}
